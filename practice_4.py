5d0261f79d25452ee985f66e74d0f974eb9eab13
Harpreet Kour <100257594+HKour2003@users.noreply.github.com>
2023-12-04 19:42:32 +0530

    Create practice_4.py

---
+++ practice_4.py
@@ -0,0 +1,9 @@
+'''The provided code stub reads and integer, , from STDIN. For all non-negative integers i<n , print i^2.'''
+
+n = int(input())  # Read an integer from STDIN
+
+for i in range(n):
+    print(i ** 2)
+
+
+
